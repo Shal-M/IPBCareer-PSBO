b692b387314912dea99c04b5670b98c4924bbd71
Faishal Mumtaz <faishal_ilkom@apps.ipb.ac.id>
2021-05-28 22:10:33 +0700

    Init Laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+    <title>IPB Career</title>
+  </head>
+  <body style="background-color:#0061A8">
+    <!--navbar -->
+    <nav class="navbar navbar-light bg-light">
+      <div class="container">
+        <a class="navbar-brand">
+          <img src="{{asset("images/ipb2.png")}}" alt="" width="80" height="80">
+        </a>
+        <ul class="nav justify-content-end">
+          <li class="nav-item">
+            <a class="nav-link active" href="#" style="font-size:20px; color:#1B1717">How it works ?</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" style="font-size:20px; color:#1B1717">About us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" style="font-size:20px; color:#1B1717">Log in</a>
+          </li>
+          <li class="nav-item">
+            <button class="btn btn-primary" type="submit">Sign Up</button>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <h1 style="font-size:38px; color:#B4D7EE; font-family: Poppins; line-height: 57px;" width:"665" height:"114">Kerja selain di bank sekarang mudah!</h1>
+    <img class="rounded float-end" src="{{asset("images/1.png")}}" alt="" width="532" height="408">
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
